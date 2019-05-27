1c604c48ee78dc0e2ce913c5dc6df0b95ab00095
ilpe <ilija.petrovic@zuehlke.com>
2019-05-27 23:59:47 +0200

    fixed terraform install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python-pip unzip
 pip install awscli
 
 echo "Installing Terraform"
-mkdir ~/bin
-cd ~/bin
-wget "https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip"
-unzip terraform_0.12.0_linux_amd64.zip
+mkdir /home/vagrant/bin
+chown vagrant:vagrant /home/vagrant/bin
+wget "https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip" -o /dev/null
+unzip terraform_0.12.0_linux_amd64.zip -d /home/vagrant/bin && rm -f terraform_0.12.0_linux_amd64.zip
